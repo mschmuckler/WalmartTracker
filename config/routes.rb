29d9fd1a4fb624578a6641dbefcac3cd9a8cc4ce
Matthew Schmuckler <mschmuckler854@gmail.com>
2017-10-29 19:05:45 -0400

    Setup webpack, add rails api boilerplate folders

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "static_pages#root"
+
+  namespace :api, defaults: { format: :json } do
+    resources :items, only: [:index, :update]
+    get "/itemspercentage", to: "items#percentage"
+  end
 end
