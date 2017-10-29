fb1706b9cb26b7a158f718026e2826054d586f48
Matthew Schmuckler <mschmuckler854@gmail.com>
2017-10-29 12:40:27 -0400

    Layout auto walmart query skeleton

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'JSON'
+
+puts "Enter a search term:"
+query = STDIN.gets.chomp
+puts "Enter the number of queries to be made over time:"
+query_count = STDIN.gets.chomp.to_i
+puts "Enter the time between each query in minutes:"
+interval_time = STDIN.gets.chomp.to_i
+
+item_counter = 1
+response = open("http://api.walmartlabs.com/v1/search?apiKey=knjkan27u9d85dnatvtf2ftw&responseGroup=full&query=cereal&numItems=25&start=#{item_counter}").read
+JSON.parse(response)["items"].each do |item|
+  puts item["brandName"]
+end
